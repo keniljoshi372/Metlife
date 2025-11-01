5821a8c48780d8a6f02574ed297d085189b2139b
keniljoshi372 <kenil.joshi@testlify.com>
2025-11-01 12:44:05 +0530

    changes

--- src/main/java/com/claimsprocessingplatform/processingplatform/model/User.java
+++ src/main/java/com/claimsprocessingplatform/processingplatform/model/User.java
@@ -1,5 +1,23 @@
-package com.claimsprocessingplatform.processingplatform.model;
+package com.example.demo.model;
 
+import lombok.Data; // <-- IMPORT LOMBOK
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+@Data // <-- ADD THIS ANNOTATION
+@Document(collection = "users") 
 public class User {
 
+    @Id
+    private String id; 
+
+    @Field("name")
+    private String fullName;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String phonenumber;
 }
